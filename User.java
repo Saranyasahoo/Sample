07cfbea211ccaebcacc59796963ae0cff4321c14
Saranyasahoo <saranyasahoo2@gmail.com>
2023-09-28 19:39:36 +0530

    Initial commit

---
+++ User.java
@@ -0,0 +1,42 @@
+package com.saranya.apis.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "User")
+public class User {
+
+	@Id
+	@Column(name = "Id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column(name = "Name")
+	private String name;
+	
+	@Column(name = "Email")
+	private String email;
+	
+	@Column(name = "Password")
+	private String password;
+	
+	@Column(name = "About")
+	private String about;
+
+}
